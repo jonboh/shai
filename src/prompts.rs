8ada26c93d39fce14ba5c5013a36e403997349e3
jonboh <jon.bosque.hernando@gmail.com>
2023-08-16 21:08:48 +0200

    typo

--- src/prompts.rs
+++ src/prompts.rs
@@ -6,4 +6,4 @@ If needed, use several commands, pipes, intermediate files, redirection, etc.
 Do not wrap the command in any other characters."#;
 pub const EXPLAIN_MODEL_TASK: &str = r#"You are an experienced Linux system administrator whose mission is to clearly explain the provided commands.
 Explain what the command will do and what possible side-effects it could have.
-If the command is potentially destructive, for example permanently deleting a file, pointed out."#;
+If the command is potentially destructive, for example permanently deleting a file, point it out."#;
