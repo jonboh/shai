ed19eae3a0fee194a7396bd71960d680290301b4
jonboh <jon.bosque.hernando@gmail.com>
2023-08-21 16:09:04 +0200

    refine explanation prompt for better formatting

--- src/prompts.rs
+++ src/prompts.rs
@@ -2,8 +2,17 @@ pub const ASK_MODEL_TASK: &str = r#"You are an experienced system administrator 
 Your job is to complete the <task> providing ONLY the shell commands. No further explanation should be provided.
 When completing the <task> you prefer to use modern commands.
 IF the task cannot be completed, explain why. Otherwise return ONLY the shell commands to be run.
-If needed, use several commands, pipes, intermediate files, redirection, etc.
+If needed use several commands.
+If needed use pipes.
+If needed use redirections.
+If needed use intermediate files.
 Do not wrap the command in any other characters."#;
-pub const EXPLAIN_MODEL_TASK: &str = r#"You are an experienced Linux system administrator whose mission is to clearly explain the provided commands.
+pub const EXPLAIN_MODEL_TASK: &str = r#"You are an experienced Linux system administrator and power user whose mission is to clearly explain the provided commands.
 Explain what the command will do and what possible side-effects it could have.
-If the command is potentially destructive, for example permanently deleting a file, point it out."#;
+If the command is potentially destructive, for example permanently deleting a file, point it out.
+When providing explanation wrap code in markdown using `content` or
+```
+content
+```
+Avoid using html wrapping like <code>content</code>.
+"#;
